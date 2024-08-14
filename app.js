834666d3a9946ae932ddee921413085d82c9f2d1
Zuitow <pc.zuit@gmail.com>
2024-08-14 14:07:58 -0300

    muitasatualizações

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const mysql = require('mysql');
-const cors = require('cors');
+const bodyParser = require("body-parser");
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const mysql = require("mysql");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
 
 // Configuração da conexão com o banco de dados MySQL
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'beglass'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "beglass",
 });
 
 // Conectando ao banco de dados
@@ -19,46 +20,87 @@ db.connect((err) => {
   if (err) {
     throw err;
   }
-  console.log('Conectado ao banco de dados MySQL');
+  console.log("Conectado ao banco de dados MySQL");
 });
 
 app.use(cors());
 app.use(bodyParser.json());
+const SECRET_KEY = "seuSegredoJWT";
 
 // Rota simples para testar a conexão
-app.get('/', (req, res) => {
-    res.send('Página Inicial');
+app.get("/", (req, res) => {
+  res.send("Página Inicial");
 });
-  
+
 // Rota de exemplo
-app.get('/sobre', (req, res) => {
-    res.send('Página Sobre');
+app.get("/sobre", (req, res) => {
+  res.send("Página Sobre");
 });
-  
+
 // Rota com parâmetro
-app.get('/usuario/:nome', (req, res) => {
-    res.send(`Olá, ${req.params.nome}!`);
+app.get("/usuario/:nome", (req, res) => {
+  res.send(`Olá, ${req.params.nome}!`);
 });
 
 // Login
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
   const { email, passcode } = req.body;
 
-  const sql = 'SELECT * FROM users WHERE email = ? AND passcode = ?';
+  const sql = "SELECT * FROM users WHERE email = ? AND passcode = ?";
   db.query(sql, [email, passcode], (err, results) => {
     if (err) {
-      return res.status(500).send('Erro ao buscar usuário');
+      return res.status(500).send("Erro ao buscar usuário");
     }
     if (results.length === 0) {
-      return res.status(404).send('Email ou senha incorretos');
+      return res.status(404).send("Email ou senha incorretos");
     }
 
-    res.status(200).send('Login bem-sucedido');
+    const user = { id: results[0].id, email: results[0].email };
+    const token = jwt.sign(user, SECRET_KEY, { expiresIn: "1h" });
+
+    res.status(200).json({ token });
   });
 });
 
+// Middleware de autenticação
+const authenticateToken = (req, res, next) => {
+  const token = req.header("Authorization")?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Token não fornecido" });
+  }
+
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Token inválido" });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+//Postagem de Reviews
+app.post("/reviews", authenticateToken, async (req, res) => {
+  try {
+    const { autor, produto, comentario, nota } = req.body;
+
+    const sql = "INSERT INTO reviews (autor, produto, comentario, nota) VALUES (?, ?, ?, ?)";
+    db.query(sql, [autor, produto, comentario, nota], (err, result) => {
+      if (err) {
+        console.error("Erro ao inserir a review:", err);
+        return res.status(500).send("Erro ao enviar Review");
+      }
+      console.log("Review inserida com sucesso!");
+      res.status(201).send("Review enviada com sucesso");
+    });
+  } catch (error) {
+    console.error("Erro ao inserir a review:", error);
+    res.status(500).json({ error: "Erro ao inserir a review" });
+  }
+});
+
 // Cadastrar Usuário
-app.post('/usuarios', (req, res) => {
+app.post("/usuarios", (req, res) => {
   const { username, email, passcode } = req.body;
 
   // Log dos dados recebidos
@@ -66,17 +108,17 @@ app.post('/usuarios', (req, res) => {
 
   // Verifique se todos os campos estão preenchidos
   if (!username || !email || !passcode) {
-    return res.status(400).send('Todos os campos são obrigatórios');
+    return res.status(400).send("Todos os campos são obrigatórios");
   }
 
-  const sql = 'INSERT INTO users (username, email, passcode) VALUES (?, ?, ?)';
+  const sql = "INSERT INTO users (username, email, passcode) VALUES (?, ?, ?)";
   db.query(sql, [username, email, passcode], (err, result) => {
     if (err) {
       // Log do erro
-      console.error('Erro ao cadastrar usuário:', err);
-      return res.status(500).send('Erro ao cadastrar usuário');
+      console.error("Erro ao cadastrar usuário:", err);
+      return res.status(500).send("Erro ao cadastrar usuário");
     }
-    res.status(201).send('Usuário cadastrado com sucesso');
+    res.status(201).send("Usuário cadastrado com sucesso");
     console.log("Usuário Cadastrado com Sucesso! " + username);
   });
 });
@@ -94,107 +136,125 @@ app.post('/usuarios', (req, res) => {
 // Envio de reviews
 app.post("/reviews", async (req, res) => {
   try {
-      const { autor, produto, comentario, nota } = req.body;
+    const { autor, produto, comentario, nota } = req.body;
 
-      const sql = "INSERT INTO reviews (autor, produto, comentario, nota) VALUES (?, ?, ?, ?)";
-      db.query(sql, [autor, produto, comentario, nota], (err, result) => {
-          if (err) {
-              console.error("Erro ao inserir a review:", err);
-              return res.status(500).send("Erro ao enviar Review");
-          }
-          console.log("Review inserida com sucesso!");
-          res.status(201).send("Review enviada com sucesso");
-      });
+    const sql = "INSERT INTO reviews (autor, produto, comentario, nota) VALUES (?, ?, ?, ?)";
+    db.query(sql, [autor, produto, comentario, nota], (err, result) => {
+      if (err) {
+        console.error("Erro ao inserir a review:", err);
+        return res.status(500).send("Erro ao enviar Review");
+      }
+      console.log("Review inserida com sucesso!");
+      res.status(201).send("Review enviada com sucesso");
+    });
   } catch (error) {
-      console.error("Erro ao inserir a review:", error);
-      res.status(500).json({ error: "Erro ao inserir a review" });
+    console.error("Erro ao inserir a review:", error);
+    res.status(500).json({ error: "Erro ao inserir a review" });
   }
 });
 
-// Coletar e analisar a nota de um produto com base no ID
-app.get('/produto/:id', async (req, res) => {
-  try {
-      const { id } = req.params;
-
-      // Consulta ao banco de dados para buscar o produto pelo ID
-      const sql = 'SELECT * FROM reviews WHERE product_id = ?';
-      db.query(sql, [id], (err, result) => {
-          if (err) {
-              console.error('Erro ao buscar o produto:', err);
-              return res.status(500).send('Erro ao buscar o produto');
-          }
-
-          if (result.length === 0) {
-              return res.status(404).send('Produto não encontrado');
-          }
-
-          const nota = result[0].nota;
-          console.log(`Nota do produto com ID ${id}: ${nota}`);
-          res.status(200).json({ nota });
-      });
-  } catch (error) {
-      console.error('Erro ao buscar o produto:', error);
-      res.status(500).json({ error: 'Erro ao buscar o produto' });
-  }
+// Endpoint para obter reviews de um produto específico pelo ID do produto
+app.get("/reviews/:id", (req, res) => {
+  const { id } = req.params;
+  const sql = "SELECT * FROM reviews WHERE produto = ?";
+  db.query(sql, [id], (err, results) => {
+    if (err) {
+      console.error("Erro ao buscar reviews:", err);
+      return res.status(500).send("Erro ao buscar reviews");
+    }
+    res.status(200).json(results);
+  });
 });
 
+// Enviar review
+// app.post("/reviews", authenticateToken, async (req, res) => {
+//   try {
+//     const { autor, produto, comentario, nota } = req.body;
+//     const userId = req.user.id; // Pegando o ID do usuário do token
 
+//     // Verifique se o usuário já enviou um review para este produto
+//     const checkExistingReview = 'SELECT * FROM reviews WHERE produto = ? AND autor = ?';
+//     db.query(checkExistingReview, [produto, userId], (err, results) => {
+//       if (err) {
+//         console.error('Erro ao verificar review existente:', err);
+//         return res.status(500).send('Erro ao verificar review existente');
+//       }
+//       if (results.length > 0) {
+//         return res.status(400).send('Você já enviou um review para este produto');
+//       }
+
+//       // Inserir o novo review
+//       const sql = "INSERT INTO reviews (autor, produto, comentario, nota) VALUES (?, ?, ?, ?)";
+//       db.query(sql, [userId, produto, comentario, nota], (err, result) => {
+//         if (err) {
+//           console.error("Erro ao inserir a review:", err);
+//           return res.status(500).send("Erro ao enviar review");
+//         }
+//         console.log("Review inserida com sucesso!");
+//         res.status(201).send("Review enviada com sucesso");
+//       });
+//     });
+//   } catch (error) {
+//     console.error("Erro ao inserir a review:", error);
+//     res.status(500).json({ error: "Erro ao inserir a review" });
+//   }
+// });
 
 // Cadastrar Administrador
-app.post('/admins', (req, res) => {
+app.post("/admins", (req, res) => {
   const { username, email, passcode } = req.body;
-  const sql = 'INSERT INTO admin (username, email, passcode) VALUES (?, ?, ?)';
+  const sql = "INSERT INTO admin (username, email, passcode) VALUES (?, ?, ?)";
   db.query(sql, [username, email, passcode], (err, result) => {
     if (err) {
-      return res.status(500).send('Erro ao cadastrar administrador');
+      return res.status(500).send("Erro ao cadastrar administrador");
     }
-    res.status(201).send('Administrador cadastrado com sucesso');
+    res.status(201).send("Administrador cadastrado com sucesso");
     console.log("Administrador Cadastrado com Sucesso! " + username);
   });
 });
 
 // Coletar dados Usuário específico
-app.get('/usuarios/pesquisar/:username', (req, res) => {
+app.get("/usuarios/pesquisar/:username", (req, res) => {
   const { username } = req.params;
-  const sql = 'SELECT * FROM usuarios WHERE username = ?';
+  const sql = "SELECT * FROM usuarios WHERE username = ?";
   db.query(sql, [username], (err, result) => {
     if (err) {
-      return res.status(500).send('Erro ao buscar usuário');
+      return res.status(500).send("Erro ao buscar usuário");
     }
     if (result.length === 0) {
-      return res.status(404).send('Usuário não encontrado');
+      return res.status(404).send("Usuário não encontrado");
     }
     res.json(result);
   });
 });
 
 // Coletar dados bebidas
-app.get('/produtos', (req, res) => {
-  const sql = 'SELECT * FROM products';
+app.get("/produtos", (req, res) => {
+  const sql = "SELECT * FROM products";
   db.query(sql, (err, results) => {
     if (err) {
-      return res.status(500).send('Erro ao buscar as produto');
+      return res.status(500).send("Erro ao buscar as produto");
     }
     res.json(results);
   });
 });
 
 // Sistema de Favoritar
-app.post('/favorites', (req, res) => {
+app.post("/favorites", (req, res) => {
   const { userId, productId } = req.body;
 
-  const query = 'INSERT INTO favorites (user_id, product_id) VALUES (?, ?)';
+  const query = "INSERT INTO favorites (user_id, product_id) VALUES (?, ?)";
   db.query(query, [userId, productId], (err, results) => {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
-    res.status(201).json({ message: 'Produto favoritado com sucesso' });
+    res.status(201).json({ message: "Produto favoritado com sucesso" });
     console.log("Produto favoritado com Sucesso!");
   });
 });
 
 // Endpoint para listar favoritos de um usuário
-app.get('/favorites/:userId', (req, res) => {
+app.get("/favorites/:userId", (req, res) => {
   const userId = req.params.userId;
 
   const query = `
@@ -212,65 +272,71 @@ app.get('/favorites/:userId', (req, res) => {
 });
 
 // Deletar Favoritos
-app.delete('/favorites', (req, res) => {
+app.delete("/favorites", (req, res) => {
   const { userId, productId } = req.body;
 
-  const query = 'DELETE FROM favorites WHERE user_id = ? AND product_id = ?';
+  const query = "DELETE FROM favorites WHERE user_id = ? AND product_id = ?";
   db.query(query, [userId, productId], (err, results) => {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
     if (results.affectedRows === 0) {
-      return res.status(404).json({ message: 'Favorito não encontrado' });
+      return res.status(404).json({ message: "Favorito não encontrado" });
     }
-    res.status(200).json({ message: 'Produto removido dos favoritos com sucesso' });
+    res
+      .status(200)
+      .json({ message: "Produto removido dos favoritos com sucesso" });
   });
 });
 
 // Cadastrar Bebida
-app.post('/produtos', (req, res) => {
+app.post("/produtos", (req, res) => {
   const { name, description, ingredientes } = req.body;
-  const sqlChecagem = 'SELECT COUNT(*) AS count FROM products WHERE name = ?';
+  const sqlChecagem = "SELECT COUNT(*) AS count FROM products WHERE name = ?";
   db.query(sqlChecagem, [name], (err, result) => {
     if (err) {
-      return res.status(500).send('Erro ao verificar o produto');
+      return res.status(500).send("Erro ao verificar o produto");
     }
     const count = result[0].count;
     if (count > 0) {
-      return res.status(400).send('Produto já cadastrado');
+      return res.status(400).send("Produto já cadastrado");
     }
-    const sqlInsercao = 'INSERT INTO products (name, description, ingredientes) VALUES (?, ?, ?)';
+    const sqlInsercao =
+      "INSERT INTO products (name, description, ingredientes) VALUES (?, ?, ?)";
     db.query(sqlInsercao, [name, description, ingredientes], (err, result) => {
       if (err) {
-        return res.status(500).send('Erro ao cadastrar produto');
+        return res.status(500).send("Erro ao cadastrar produto");
       }
-      res.status(201).send(`Produto cadastrado com Sucesso 😁 ${name}, Que interessante!`);
+      res
+        .status(201)
+        .send(`Produto cadastrado com Sucesso 😁 ${name}, Que interessante!`);
     });
   });
 });
 
-// Específico pelo ID
-app.get('/produtos/pesquisar/id/:id', (req, res) => {
+// Endpoint para obter um produto específico pelo ID
+app.get("/produtos/:id", (req, res) => {
   const { id } = req.params;
-  const sql = 'SELECT * FROM products WHERE id = ?';
+  const sql = "SELECT * FROM products WHERE id = ?";
   db.query(sql, [id], (err, result) => {
     if (err) {
-      return res.status(500).send('Erro ao buscar produto');
+      console.error("Erro ao buscar produto:", err);
+      return res.status(500).send("Erro ao buscar produto");
     }
     if (result.length === 0) {
-      return res.status(404).send('Produto não encontrado');
+      return res.status(404).send("Produto não encontrado");
     }
-    res.json(result);
+    res.json(result[0]); // Retorna um único objeto
   });
 });
 
 // Específico pelo nome
-app.get('/produtos/pesquisar/nome/:nome', (req, res) => {
+app.get("/produtos/pesquisar/nome/:nome", (req, res) => {
   const { nome } = req.params;
-  const sql = 'SELECT * FROM products WHERE name LIKE ?';
+  const sql = "SELECT * FROM products WHERE name LIKE ?";
   db.query(sql, [`%${nome}%`], (err, results) => {
     if (err) {
-      return res.status(500).send('Erro ao buscar produto');
+      return res.status(500).send("Erro ao buscar produto");
     }
     res.json(results);
   });
